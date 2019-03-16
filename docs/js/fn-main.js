f9c49e8cc50834b72a20b9334184bc7a32dc8b73
Lewiz Acosta <mp3edw@gmail.com>
2019-03-16 15:28:09 -0500

    Update fn-main.js

--- docs/js/fn-main.js
+++ docs/js/fn-main.js
@@ -83,7 +83,7 @@
                    componentHandler.upgradeDom();
                },
                success: function (res) {
-                    res.forEach(function(i, v) {
+                    res.vidInfo.forEach(function(i, v) {
                         console.log(v);
                     });
                     // var uridl = decodeURI(res.vidInfo);
